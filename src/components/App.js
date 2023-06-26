252f73fbff7d5814f6e3d54d7231c59de754bf68
CrisMuniz <cristinamuniz85@gmail.com>
2023-06-26 10:16:01 +0200

    add functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,67 @@
+import { useEffect, useState } from 'react';
 import '../styles/App.scss';
 
 function App() {
+  const [characterList, setCharacterList] = useState([]);
+  const [quoteSeach, setQuoteSeach] = useState ('');
+  const [char, setChar] = useState('');
+
+useEffect(() => {
+  fetch (
+  'https://beta.adalab.es/curso-intensivo-fullstack-recursos/apis/quotes-friends-tv-v1/quotes.json'
+  )
+  .then((response) =>response.json())
+  .then((data) =>{
+    setCharacterList(data)
+  })
+})
+const handleInputSearch = (ev) => {
+  setQuoteSeach(ev.target.value);
+}
+const handleChar = (ev) => {
+  if (ev.target.value === 'todos') {
+    setChar('')
+  }else{
+  setChar(ev.target.value)
+}
+}
+const renderCharacterList = () => {
+  const filteredList = characterList.filter((eachItem) =>
+  eachItem.quote.toLowerCase().includes(quoteSeach.toLocaleLowerCase()) &&
+  eachItem.character.includes(char))
+  return filteredList.map((eachCharacter) => (
+    <li>
+    <p>{eachCharacter.quote}</p>
+    <label htmlFor="">{eachCharacter.character}</label>
+  </li>
+  ))
+};
+
+
   return (
     <div>
-   <h1>Hola Mundo</h1>
+   <h1>Frases De Frerinds</h1>
+   <header>
+    <form action="">
+      <label htmlFor="">Filtrar por Frase</label>
+      <input type="text" name='search' value={quoteSeach} onInput={handleInputSearch}/>
+      <label htmlFor="">Filtrar por personaje</label>
+      <select name="char" id="char" onChange={handleChar}>
+        <option value="todos">todos</option>
+        <option value="Ross">Ross</option>
+        <option value="Monica">Monica</option>
+        <option value="Joey">Joey</option>
+        <option value="Phoebe">Phoebe</option>
+        <option value="Chandler">Chandler</option>
+        <option value="Rachel">Rachel</option>
+      </select>
+    </form>
+   </header>
+   <main>
+    <ul>
+     {renderCharacterList()}
+    </ul>
+   </main>
     </div>
   );
 }
